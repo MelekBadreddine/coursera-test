c3f567a8a4f24f8b0889efc31099fe78c01fa6d9
MelekBadreddine <119127830+MelekBadreddine@users.noreply.github.com>
2022-12-20 16:04:01 +0100

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var speakWord = "Hello";
-    var helloSpeaker = {};
-    helloSpeaker.speak = function() {
-        console.log(speakWord+ " " + helloSpeaker.name);
-    };
-window.helloSpeaker=helloSpeaker;
-})(window);
