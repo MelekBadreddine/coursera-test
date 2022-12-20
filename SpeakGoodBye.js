b17794e139cd3e01dbeed66a092326eee54822d1
MelekBadreddine <119127830+MelekBadreddine@users.noreply.github.com>
2022-12-20 16:03:54 +0100

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var speakWord = "Good Bye";
-    var byeSpeaker = {};
-    byeSpeaker.speak = function() {
-        console.log(speakWord + " " + byeSpeaker.name);
-    };
-window.byeSpeaker=byeSpeaker;
-})(window);
